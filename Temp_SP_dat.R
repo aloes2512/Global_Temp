614b5c9eb4eb04bdee0899f6a4e20eb6f740ebb9
aloes2512 <alf.loeffler@icloud.com>
2022-03-18 16:24:20 +0100

    join SP and Temp timeseries to analyse interactions

--- Temp_SP_dat.R
+++ Temp_SP_dat.R
@@ -15,6 +15,7 @@ dat_path<- "/Users/alfloeffler/Desktop/Klima_Energiewende/Daten/SN_d_tot_V2.0.cs
 #variables of "SN_d_tot_V2.0.csv":
 #year, month, day, decimal.year, SNvalue , SNerror, Nb_observations
 # names(sunspot.daily)
+require(lubridate)
 sunspot.daily<-read.csv2(dat_path)%>% 
   dplyr::select(Year,Month,Day,Daily.Nr)%>% 
   mutate(datum=paste0(Year,"-",Month,"-",Day),datum=ymd(datum))
@@ -31,27 +32,27 @@ head(Temp_SP_dat,1)
 require(mgcv)
 require(mgcViz)
 Temp_SP_mdl<- gam(data=Temp_SP_dat,formula=Temp~ s(datum,k=12)+s(SP,k=12), method="REML")
+summary(Temp_SP_mdl)
 Temp_SP_mdl<- getViz(Temp_SP_mdl)
 summary(Temp_SP_mdl)
 plot(sm(Temp_SP_mdl,2))
 o <- plot( sm(Temp_SP_mdl,1) )
 o+l_fitLine(colour = "red") + 
   l_ciLine(mul = 5, colour = "blue", linetype = 2) 
-str(o)
 plot(sm(Temp_SP_mdl,2))
 require(broom)
 Temp_SP_fit<-Temp_SP_mdl%>%augment()%>%
   dplyr::select(Temp,Tmp_fit=.fitted,SP,Tmp_se=.se.fit)
 Temp_SP_fit$Datum<-Temp_SP_dat$Datum
-Temp_SP_fit%>% ggplot(aes(x=Datum))+
-  geom_line(aes(y=Tmp_fit))+
-  geom_smooth(aes(y= SP/10),col= "red")
+
+#====================
 Sunspots_daily<-Temp_SP_fit%>% ggplot(aes(x=Datum))+
   geom_smooth(aes(y=SP)) +
   ggtitle("Mean Daily Sunspots (SP)",
           subtitle = "Source:https://wwwbis.sidc.be/silso/")+
   labs(x= "", y= "Daily-Counted-Sunspots")
 ggsave(Sunspots_daily,"Sunspots_daily.png")
+#===============
 Temp_SP_plt<-Temp_SP_fit%>%ggplot(aes(x=SP))+
   geom_point(aes(y= Tmp_fit),size= 0.1)+
   geom_smooth(aes(y= Tmp_fit),col= "red",linetype=2,size=0.5)+
@@ -63,3 +64,28 @@ Temp_SP_plt+geom_smooth(data=Temp_SP_fit,
                         method = "lm",
                         formula = y ~ x,
                         col="red")
+####==============
+Temp_SP_viz<-function(dfr,x=Datum,y1=Tmp_fit,y2=SP){
+  dfr<-dfr%>%dplyr::select(Datum,Tmp_fit,SP)
+  from<-range(dfr$Datum)[1]
+  to<-range(dfr$Datum)[2]
+  xab<-lm(dfr,formula=Tmp_fit~SP)$coefficients
+  plt<-dfr%>%ggplot(aes(x=SP))+
+    geom_point(aes(y= Tmp_fit),size= 0.1)+
+    geom_smooth(aes(y= Tmp_fit),col= "red",linetype=2,size=0.5)+
+    geom_abline(intercept=xab[1],slope = xab[2],col = "red")+
+    ggtitle("Mean-Daily-Temperature~ Sunspots(counts)
+  Central-England",
+            subtitle=paste(from,to))+
+    labs(x=" Sunspots",y= expression(Temperature~(degree*C)))
+  return(plt)
+}
+# select by date
+plt<-Temp_SP_viz(Temp_SP_fit)
+Temp_SP_select_l<-Temp_SP_fit%>%subset(Datum<ymd("1900-01-01"))
+Temp_SP_viz(Temp_SP_select_l)
+Temp_SP_select_h<-Temp_SP_fit%>%subset(Datum>=ymd("2010-01-01"))
+Temp_SP_viz(Temp_SP_fit%>%subset(Datum>=ymd("1845-01-01")&
+              Datum<=ymd("1854-12-31")))
+Temp_SP_viz(Temp_SP_fit%>%subset(Datum>=ymd("1895-01-01")&
+                                   Datum<=ymd("2004-12-31")))
