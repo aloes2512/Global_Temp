e3d14b5bb38db260ae18abd2d209773226a48c9c
aloes2512 <alf.loeffler@icloud.com>
2022-06-23 15:12:55 +0200

    Minor changes to improve visibility

--- CO2_lin_trend.R
+++ CO2_lin_trend.R
@@ -1,10 +1,11 @@
 # linear regression model
 CO2_data.lst<- readRDS("~/projects/Global_Temp/data/NOAA_data.rds")
 summary(CO2_data.lst)
+CO2_data.lst%>% map(summary)
 length(CO2_data.lst)# 20
 require(tidyverse)
 require(mgcv)
-CO2_regr_mdl<-CO2_data.lst%>%map(~gam(.$CO2~.$datetime,data = .))
+CO2_regr_mdl<-CO2_data.lst%>%map(~gam(.$CO2~.$datetime,data = .,method = "REML"))
 names(CO2_regr_mdl)
 require (broom)
 CO2_fitted<-CO2_regr_mdl%>% map(augment)%>%
@@ -12,8 +13,6 @@ CO2_fitted<-CO2_regr_mdl%>% map(augment)%>%
                               datetime=`.$datetime`,
                               CO2_fit= .fitted)%>%
                           map(dplyr::select,datetime,CO2,CO2_fit)
-CO2_fitted[[1]]
-
 
 # combine with site parameters
 station_sites<-readRDS("~/projects/Global_Temp/data/station_sites.rds")
@@ -27,7 +26,7 @@ nmdt<-function(x){
 }
 (nmdt(CO2_data.lst[[1]]))
 CO2_data.<-CO2_data.lst%>% map(nmdt)%>%map(function(x){ x<- x%>%dplyr::select(dttm,site,CO2, lat,lon,alt)})
-CO2_data.[[19]]
+CO2_data.[[20]]
 CO2_data<-CO2_data.%>% map_dfr(bind_rows,.id = "tibble") #
 # umwandeln von datetime in numeric
 CO2_fit_nmdt<-CO2_fitted%>%map(nmdt)%>% map(dplyr::select,-datetime)%>%
@@ -41,6 +40,9 @@ rownames(CO2_fit_nmdt)%>% tail()
 CO2_fit.tbl<-CO2_fit_nmdt%>% 
   mutate(datetime=as.POSIXct(dttm,origin="1970-01-01",tz="UTC"))%>%
   dplyr::select(-dttm)
+
+
+summary(CO2_fit.tbl) # outlier CO2==195.6 @ Schauinsland replaced 
 CO2_lin_plt<-CO2_fit.tbl%>% group_by(site)%>%
   ggplot(aes(x=datetime,y=CO2_fit,col= site))+
   geom_line()
@@ -63,8 +65,13 @@ Storhofdi(ICE),Summit(SUM),Ny-Alesund(Svalbard(ZEP)) ")+
 #plot southern stations trend
 south_CO2_lin_plt<-south_CO2_fit%>% group_by(site)%>%
   ggplot(aes(x=datetime,y=CO2_fit,col= site))+
+  geom_point(aes(x=datetime,y= CO2,col=site))+
   geom_line(alpha= 0.4)
 
-south_CO2_lin_plt+ggtitle("CO2-linear trend",
+south_CO2_lin_plt<-south_CO2_lin_plt+ggtitle("CO2-linear trend",
             subtitle = "2 sites South Pole (SPO)&Palmer (PSA)")+
                             labs(x="",y= "CO2 [ppm]")
+#calculate residuals CO2-CO2_fit
+CO2_fit_nmdt%>% head()
+CO2_lin.resd<-CO2_fit_nmdt%>% mutate(CO2_resd= CO2-CO2_fit)
+saveRDS(CO2_lin.resd,"~/projects/Global_Temp/data/CO2_lin.resd.rds")
